9ae6911086dcd36f755dfeeb8b13badc835404d3
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-10-07 20:03:48 +0900

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+javascript:
+(function(){var goaway=".ytp-chrome-top,.ytp-chrome-bottom{display:none;}";if("\v"=="v"){document.createStyleSheet().cssText=goaway}else{var tag=document.createElement("style");tag.type="text/css";document.getElementsByTagName("head")[0].appendChild(tag);tag[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"]=goaway}})();
